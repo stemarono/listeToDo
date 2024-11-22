2044549fabe8ea6407ec9824814ed4a0ab3525e0
stemarono <stephanielacaze93@gmail.com>
2024-11-22 11:07:46 +0100

    début du projet création de html et rajout de couleurs dans tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js}" ,"./index.html"],
+  content: ["./src/**/*.{JS}" ,"./index.html"],
   theme: {
-    extend: {  },
+    extend: { 
+      colors:{
+        'blue-dark':'#22587C',
+        'blue-clear':'#38a7a6',
+        'blue-green':'#59cd97',
+        'green-clear':'#83ee99',
+        'green-pastel':'#cbfacf',
+      }
+     },
   },
   plugins: [],
 }
